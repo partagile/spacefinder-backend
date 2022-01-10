52df6e8deb577c6cd8575cafc6b708d47d058e18
Thushyanthan A <hello@thushyanthan.com>
2022-01-10 13:05:13 -0800

    creating lambda and apigateway integration; REST tests

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,10 +1,29 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Code, Function as LambdaFunction, Handler, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path'
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 
 
 export class SpaceStack extends Stack {
 
+    private api = new RestApi(this, 'SpaceApi' )
+
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
+
+
+        const helloLambda = new LambdaFunction(this, 'helloLambda', {
+            runtime: Runtime.NODEJS_14_X,
+            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+            handler: 'hello.main'
+        })
+
+
+        // lambda integration with api gateway
+        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaResource = this.api.root.addResource('hello');
+        helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+
     }
 }
