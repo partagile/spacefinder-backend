f392587ebb79a56243a4a3296be30f65baae1673
Thushyanthan A <hello@thushyanthan.com>
2022-01-10 13:34:22 -0800

    creating dynamodb table; modifying tsconfig for relaxed checks on propertyInitialization

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -3,16 +3,22 @@ import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Handler, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path'
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import { GenericTable } from './GenericTable';
 
 
 export class SpaceStack extends Stack {
 
-    private api = new RestApi(this, 'SpaceApi' )
+    private api = new RestApi(this, 'SpaceApi');
+    private spacesTable = new GenericTable(
+        'SpacesTable',
+        'spaceId',
+        this
+    )
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
 
-
+        
         const helloLambda = new LambdaFunction(this, 'helloLambda', {
             runtime: Runtime.NODEJS_14_X,
             code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
