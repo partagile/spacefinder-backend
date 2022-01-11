271cd26e16f2cd03d8fa2cc3942d0bf8203d9617
Thushyanthan A <hello@thushyanthan.com>
2022-01-11 12:54:40 -0800

    building and integrating readLambda with own query

--- services/SpacesTable/Read.ts
+++ services/SpacesTable/Read.ts
@@ -2,7 +2,8 @@ import { DynamoDB } from "aws-sdk";
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
 const dbClient = new DynamoDB.DocumentClient();
-const TABLE_NAME = process.env.TABLE_NAME
+const TABLE_NAME = process.env.TABLE_NAME;
+const PRIMARY_KEY = process.env.PRIMARY_KEY;
 
 async function handler(
     event: APIGatewayProxyEvent,
@@ -14,10 +15,28 @@ async function handler(
     }
 
     try {
-        const queryResponse = await dbClient.scan({
-            TableName: TABLE_NAME!
-        }).promise()
-        result.body = JSON.stringify(queryResponse)
+        if (event.queryStringParameters) {
+            if (PRIMARY_KEY! in event.queryStringParameters) {
+                const keyParam = event.queryStringParameters[PRIMARY_KEY!];
+                const queryResponse = await dbClient.query({
+                    TableName: TABLE_NAME!,
+                    KeyConditionExpression: '#expression1 = :expressionValue1',
+                    ExpressionAttributeNames: {
+                        '#expression1': PRIMARY_KEY!
+                    },
+                    ExpressionAttributeValues: {
+                        ':expressionValue1': keyParam
+                    }
+                }).promise();
+                result.body = JSON.stringify(queryResponse);
+            }
+        } else {
+            const queryResponse = await dbClient.scan({
+                TableName: TABLE_NAME!
+            }).promise()
+            result.body = JSON.stringify(queryResponse)            
+        }
+
     } catch (e: unknown) {
         const error = e as Error;
         result.body = error.message;
---
+++ test/local-playground/Query.test.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { handler } from '../../services/SpacesTable/Read';
+
+const event: APIGatewayProxyEvent = {
+    queryStringParameters: {
+        spaceId: '8bd4a9e4-380a-4b83-bf74-90d3d22d9409'
+    }
+} as any;
+
+
+const result = handler(event as any, {} as any).then((apiResult)=>{
+    const items = JSON.parse(apiResult.body);
+    console.log('for debug breakpoint')
+});
