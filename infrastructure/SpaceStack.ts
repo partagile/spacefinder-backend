b596d97e82e5b1222d76f6adcbb238a9bb847824
Thushyanthan A <hello@thushyanthan.com>
2022-01-10 16:23:48 -0800

    removed js lambda; npm i aws-sdk; setup launch.json and test folders for local debugging

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,15 +1,16 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Code, Function as LambdaFunction, Handler, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { Code, Function as LambdaFunction, Handler, Runtime } from 'aws-cdk-lib/aws-lambda'
 import { join } from 'path'
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
-import { GenericTable } from './GenericTable';
+import { GenericTable } from './GenericTable'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
 
 
 export class SpaceStack extends Stack {
 
-    private api = new RestApi(this, 'SpaceApi');
+    private api = new RestApi(this, 'SpaceApi')
     private spacesTable = new GenericTable(
         'SpacesTable',
         'spaceId',
@@ -18,22 +19,20 @@ export class SpaceStack extends Stack {
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
-
         
-        const helloLambda = new LambdaFunction(this, 'helloLambda', {
-            runtime: Runtime.NODEJS_14_X,
-            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-            handler: 'hello.main'
-        })
-
         const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
             entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
             handler: 'handler'
         })
 
+        const s3ListingBucketPolicy = new PolicyStatement();
+        s3ListingBucketPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListingBucketPolicy.addResources('*'); // testing only
+
+        helloLambdaNodeJs.addToRolePolicy(s3ListingBucketPolicy);
 
         // lambda integration with api gateway
-        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
         const helloLambdaResource = this.api.root.addResource('hello');
         helloLambdaResource.addMethod('GET', helloLambdaIntegration);
 
--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,10 +1,16 @@
 import { v4 } from 'uuid'
+import { S3 } from 'aws-sdk'
+
+const s3Client = new S3();
 
 
 async function handler(event: any, context: any) {
+    const buckets = await s3Client.listBuckets().promise();
+    console.log('Event hit:');
+    console.log(event);
     return {
         statusCode: 200,
-        body: 'Hello from TS lambda, Partagile!' + v4()
+        body: 'Listing S3 buckets: ' + JSON.stringify(buckets.Buckets)
     }
 }
 
