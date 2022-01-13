1030bd5b85d91faa7448a7d7ee0aae060de3bf37
Thushyanthan A <hello@thushyanthan.com>
2022-01-12 19:32:35 -0800

    testing JWT tokens with cognito management console

---
+++ test/Auth/config.ts
@@ -0,0 +1,10 @@
+
+
+
+export const config = {
+    REGION: 'us-west-2',
+    USER_POOL_ID: 'us-west-2_AcbfuRm7L',
+    APP_CLIENT_ID: '7t6kd6lplr73273k0oesq8gd7t',
+    TEST_USER_NAME: 'hello',
+    TEST_USER_PASSWORD: '5EApCsq&'
+}
