49a6e7dfa771447fb2ec3bdae2df3fa88902e1a0
Thushyanthan A <hello@thushyanthan.com>
2022-01-13 13:13:47 -0800

    added default authenticated and unauthenticated roles plus additional adminRole

--- infrastructure/auth/IdentityPoolWrapper.ts
+++ infrastructure/auth/IdentityPoolWrapper.ts
@@ -1,5 +1,6 @@
 import { CfnOutput } from "aws-cdk-lib";
 import { UserPool, UserPoolClient, CfnIdentityPool } from "aws-cdk-lib/aws-cognito";
+import { FederatedPrincipal, Role } from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 import { config } from "process";
 
@@ -12,6 +13,8 @@ export class IdentityPoolWrapper{
     private userPoolClient: UserPoolClient;
 
     private identityPool: CfnIdentityPool;
+    private authenticatedRole: Role;
+    private unauthenticatedRole: Role;
 
     constructor (scope: Construct, userPool: UserPool, userPoolClient: UserPoolClient){
         this.scope = scope;
@@ -22,6 +25,7 @@ export class IdentityPoolWrapper{
 
     private initialize(){
         this.initializeIdentityPool();
+        this.initializeRoles;
     }
 
     private initializeIdentityPool(){
@@ -37,6 +41,22 @@ export class IdentityPoolWrapper{
             value: this.identityPool.ref
         })
 
+    }
+
+    private initializeRoles(){
+        this.authenticatedRole = new Role(this.scope, 'CognitoDefaultAuthenticatedRole', {
+            assumedBy: new FederatedPrincipal('cognito-identity.amazonaws.com', {
+                StringEquals: {
+                    'cognito-identity.amazonaws.com:aud': this.identityPool.ref
+                },
+                'ForAnyValue:StringLike': {
+                    'cognito-identity.amazonaws.com:aud': 'authenticated'
+                }
+            },
+            'sts:AssumeRoleWithWebIdentity'
+            )
+
+        });
     }
 
 }
