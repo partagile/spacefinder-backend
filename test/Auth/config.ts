b012faf7634504cc11e3713f87609f1e9379a0c8
Thushyanthan A <hello@thushyanthan.com>
2022-01-19 20:58:44 -0800

    updated config with new deployment info

--- test/Auth/config.ts
+++ test/Auth/config.ts
@@ -3,9 +3,9 @@
 
 export const config = {
     REGION: 'us-west-2',
-    USER_POOL_ID: 'us-west-2_fBM44XXy9',
-    APP_CLIENT_ID: '5psn11jv28hloisnss1opcne6u',
-    IDENTITY_POOL_ID: 'us-west-2:5cc69f83-0552-465e-8a4f-ee2968530367',
+    USER_POOL_ID: 'us-west-2_ntoyPoIN1',
+    APP_CLIENT_ID: '6otvml2idkamc9e2pn6l19q9lv',
+    IDENTITY_POOL_ID: 'us-west-2:d033c78d-9b5f-434c-a9f9-fe8186f915a5',
     TEST_USER_NAME: 'hello',
     TEST_USER_PASSWORD: '5EApCsq&'
 }
